cead197dd46027c97c0162e3bd40d91dc6a2437b
GlaireDaggers <glairedaggers@gmail.com>
2025-02-12 13:03:21 -0800

    One day I will get the syntax of a Windows command correct on the first try, but today is not that day

--- src/publish-win-x64.bat
+++ src/publish-win-x64.bat
@@ -1,4 +1,4 @@
 dotnet publish -c Release -r win-x64 --output .\publish\dreambox-win-x64\ DreamboxVM.csproj || exit /b
 robocopy .\content .\publish\dreambox-win-x64\content /s
 robocopy .\native-libs\win-x64 .\publish\dreambox-win-x64
-xcopy /y .\icon.qoi .\publish\dreambox-win-x64\icon.qoi
+xcopy /f /y .\icon.qoi .\publish\dreambox-win-x64\
