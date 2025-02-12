d50eb0de238451371a3160a270ee39f7327fdebe
GlaireDaggers <glairedaggers@gmail.com>
2025-02-11 17:03:24 -0800

    Fixed windows build script not copying native DLLs

--- src/publish-win-x64.bat
+++ src/publish-win-x64.bat
@@ -1,4 +1,4 @@
 dotnet publish -c Release -r win-x64 --output .\publish\dreambox-win-x64\ DreamboxVM.csproj || exit /b
 robocopy .\content .\publish\dreambox-win-x64\content /s
-robocopy .\native-libs\win-x64\*.dll .\publish\dreambox-win-x64
+robocopy .\native-libs\win-x64 .\publish\dreambox-win-x64
 tar -cvz -f .\publish\dreambox-win-x64.tar.gz -C .\publish dreambox-win-x64
