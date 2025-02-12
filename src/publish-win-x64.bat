cfe6b11893546bf791e6fe77da4e81e35c8c22ed
GlaireDaggers <glairedaggers@gmail.com>
2025-02-11 16:45:00 -0800

    Fixing robocopy options in windows build script

--- src/publish-win-x64.bat
+++ src/publish-win-x64.bat
@@ -1,4 +1,4 @@
 dotnet publish -c Release -r win-x64 --output .\publish\dreambox-win-x64\ DreamboxVM.csproj || exit /b
-robocopy .\content .\publish\dreambox-win-x64 /y /s || exit /b
-robocopy .\native-libs\win-x64\*.dll .\publish\dreambox-win-x64 /y || exit /b
+robocopy .\content .\publish\dreambox-win-x64 /s || exit /b
+robocopy .\native-libs\win-x64\*.dll .\publish\dreambox-win-x64 || exit /b
 tar -cvz -f .\publish\dreambox-win-x64.tar.gz -C .\publish dreambox-win-x64 || exit /b
